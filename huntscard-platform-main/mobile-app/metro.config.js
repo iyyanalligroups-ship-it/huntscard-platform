c681498b3527cc2161508af056d391e36a78a035
iyyanalligroups-ship-it <iyyanalligroups@gmail.com>
2026-07-23 14:34:48 +0530

    mobile app folder

--- huntscard-platform-main/mobile-app/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add glb/gltf 3D model support
-config.resolver.assetExts.push('glb', 'gltf');
-
-module.exports = config;
-
